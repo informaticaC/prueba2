44919d9a1ea021c3ddd29211fa28414145c0ebb9
Gabriel <pesoa01@gmail.com>
2022-07-13 08:55:42 -0300

    First commit ever! 7/13/2022

---
+++ Main.java
@@ -0,0 +1,15 @@
+//Primer programa en Java 30/06/2022
+
+
+public class Main{
+	public static void main(String[] args){
+		System.out.println("Hello World");
+
+		for(int i = 0; i<10;i++){
+			System.out.println(i);
+		}
+	}
+}
+
+	
+	
